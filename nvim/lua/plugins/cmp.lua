3bcda8aca6aa987891ada5a1074ff3d27249303c
sandipduley <sandipp0987rahul@gmail.com>
2025-11-27 16:34:16 +0530

    added: tiny-inline-diagnostic support

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,20 +1,27 @@
 return {
-  "hrsh7th/nvim-cmp", -- Main completion engine
+  "hrsh7th/nvim-cmp",
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",      -- LSP completions
-    "hrsh7th/cmp-buffer",        -- Buffer completions
-    "hrsh7th/cmp-path",          -- Path completions
-    "hrsh7th/cmp-cmdline",       -- Command line completions
-    "L3MON4D3/LuaSnip",          -- Snippet engine
-    "saadparwaiz1/cmp_luasnip",  -- Snippet completions
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets", -- Extra snippets
   },
+
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
+    -- 🔄 Load VSCode-style snippets (friendly-snippets)
+    ------------------------------------------------------------------
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    ------------------------------------------------------------------
     -- 🔁 Persistent toggle state
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
     local cmp_state_file = vim.fn.stdpath("data") .. "/cmp_toggle_state.txt"
     local cmp_enabled = true
 
@@ -23,9 +30,9 @@ return {
       cmp_enabled = (state == "true")
     end
 
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
     -- ⚙️ Main cmp setup
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
     cmp.setup({
       enabled = function()
         return cmp_enabled
@@ -37,21 +44,71 @@ return {
         end,
       },
 
+      ----------------------------------------------------------
+      -- 🖼️ Beautiful bordered completion/documentation windows
+      ----------------------------------------------------------
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      ----------------------------------------------------------
+      -- 🎨 Formatting for clean menus
+      ----------------------------------------------------------
+      formatting = {
+        format = function(entry, item)
+          item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip  = "[Snip]",
+            buffer   = "[Buf]",
+            path     = "[Path]",
+          })[entry.source.name]
+          return item
+        end,
+      },
+
+      ----------------------------------------------------------
+      -- ⌨️ Key mappings
+      ----------------------------------------------------------
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
 
-        -- Change confirm key to Shift+Enter
+        -- Confirm: Shift + Enter
         ["<S-CR>"] = cmp.mapping.confirm({ select = true }),
 
-        -- Optional: prevent Enter from confirming
+        -- Enter = newline (not confirm!)
         ["<CR>"] = cmp.mapping(function(fallback)
-          fallback() -- just insert newline
+          fallback()
         end, { "i", "s" }),
+
+        -- Tab / Shift-Tab for snippet navigation
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+
+        ["<S-Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
       }),
 
+      ----------------------------------------------------------
+      -- 🔌 Sources (priority-ordered)
+      ----------------------------------------------------------
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
@@ -61,9 +118,9 @@ return {
       }),
     })
 
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
     -- 🎚️ Toggle autocomplete shortcut
-    ----------------------------------------------------------------
+    ------------------------------------------------------------------
     vim.keymap.set("n", "<leader>ta", function()
       cmp_enabled = not cmp_enabled
       vim.fn.writefile({ tostring(cmp_enabled) }, cmp_state_file)
@@ -76,5 +133,4 @@ return {
     end, { desc = "Toggle autocomplete" })
   end,
 }
-
 
