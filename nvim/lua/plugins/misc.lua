b11b072d166057b3ccd889a991b9c6a8031e5286
sandipduley <sandipp0987rahul@gmail.com>
2025-12-13 01:23:51 +0530

    ADDED: nvim-surround plugin

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -39,5 +39,17 @@ return {
     end,
   },
 
+    -- Add/delete/change surrounding pairs
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  }
+
 }
 
