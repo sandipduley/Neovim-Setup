0c1eb2876d799d8531ae50f765cbe85486321f57
sandipduley <sandipp0987rahul@gmail.com>
2025-12-12 22:28:04 +0530

    UPDATE: yazi.lua config

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -8,22 +8,22 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>-",
+      "\\",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
-    {
-      -- Open in the current working directory
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
+    -- {
+    --   -- Open in the current working directory
+    --   "<leader>cw",
+    --   "<cmd>Yazi cwd<cr>",
+    --   desc = "Open the file manager in nvim's working directory",
+    -- },
+    -- {
+    --   "<c-up>",
+    --   "<cmd>Yazi toggle<cr>",
+    --   desc = "Resume the last yazi session",
+    -- },
   },
   ---@type YaziConfig | {}
   opts = {
@@ -41,3 +41,4 @@ return {
     vim.g.loaded_netrwPlugin = 1
   end,
 }
+
